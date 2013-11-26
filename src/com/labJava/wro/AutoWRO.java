48cae1d5b39c28248e6de56b5bc80ee710c6a3de
Maximiliano <mmcontini08@gmail.com>
2013-11-26 20:27:10 -0300

    Segundo commit

--- src/com/labJava/wro/AutoWRO.java
+++ src/com/labJava/wro/AutoWRO.java
@@ -1,8 +1,5 @@
 package com.labJava.wro;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 public class AutoWRO {
 	
 	private MotorWRO motor;
@@ -16,23 +13,5 @@ public class AutoWRO {
 	
 	public MotorWRO getMotor() {return motor;}
 	public int getTamanioMotor() {return tamanioMotor;} 
-	
-	private void writeObject(ObjectOutputStream os) {
-		try {
-			os.defaultWriteObject();
-			os.writeInt(motor.getTamanioMotor());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void readObject(ObjectInputStream is) {
-		try {
-			is.defaultReadObject();
-			motor = new MotorWRO(is.readInt());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+		
 }
